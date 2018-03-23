d983d756d81f2eccfb19cd41afb87628945f1cd1
samaea <32162601+samaea@users.noreply.github.com>
2018-03-23 12:22:33 +0000

    Create traffic-manager.sh

---
+++ traffic-manager.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+echo Please input your team.
+read teamName
+
+teamName=$teamName$RANDOM
+az group create -n AksChallengeTrafficManager -l westeurope
+az network traffic-manager profile create -n aksChallenge2 -g AksChallengeTrafficManager --routing-method Priority --unique-dns-name $teamName
+az network traffic-manager endpoint create --profile-name aksChallenge -n Primary -g aksChallengeTrafficManager --type externalEndpoints --target "samofthing.com" --priority 1
+
+#Now trigger logic app to register deployment for tracking
+curl -d '{"URL" :"$teamName"}' -H "Content-Type: application/json" -X POST 'https://prod-25.northcentralus.logic.azure.com:443/workflows/5951e14e586c407c9750e2b231760b62/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=3NbKbrZyTZkfxpdK4MLGAKdoVbLMTMc3Iz71-AobiUI'
